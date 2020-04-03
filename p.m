7ebb6498007b1311e6de7502f679a1e20e9b1252
prachigupta234 <58383763+prachigupta234@users.noreply.github.com>
2020-04-03 11:07:30 +0530

    Add files via upload

---
+++ p.m
@@ -0,0 +1,34 @@
+clear;close all;clc%clear and close figures
+%suppose the file from which we are loading data is filename and it has data such that the last colum has value of y and other have features. 
+data=load('filename.txt');%load data
+X=data(:,1:size(X,2)-1);
+Y=data(:,size(X,2);
+m=length(y);
+%Normailising fetures
+mean=mean(X);
+standev=std(X);
+for i=1:size(X,2)
+  a(:,1)=X(:,i)-mean(1,i);
+  Xnormalised(:,i)=a(:,1)/standev(1,i);
+end;
+X=Xnormalised;
+X=[ones(m,1) X];
+%assuming some value of alpha and the number of iterations
+alpha=.001;
+numiter=15000;%number of iterations
+%let all the theta be zero initially
+theta=zeros(size(X,2),1);
+%calculating theta and cost function
+for iter=1:numiter
+  for i=1:size(X,2)	
+  c(1,i)=(((X*theta)-y)'*X(:,i));
+  end;
+  for i=1:size(X,2)
+  theta(i,1) = theta(i,1)-(alpha*(1/m)*c(1,i));
+  end;
+  J(iter)=(1/(2*m))*(((X*theta)-y)'*((X*theta)-y));
+end;
+fprintf('Theta computed:\n');
+fprintf('%f\n',theta);
+%this all also be done for not choosing alpha by our own even calculting alpha which is best that can be done by normal equations
+%and by this we can also plot some graphs and we can also predict the y foe any value of all the features.
---
+++ p2.m
@@ -0,0 +1,20 @@
+clear;close all;clc%clear and close figures
+%suppose the file from which we are loading data is filename and it has data such that the last colum has value of y and other have features. 
+data=load('filename.txt');%load data
+X=data(:,1:size(X,2)-1);
+Y=data(:,size(X,2);
+m=length(y);
+X=[ones(m,1) X];
+%assuming some value of alpha and the number of iterations
+alpha=.001;
+numiter=15000;%number of iterations
+lambda=2.3;%Regularisation parameter lambda
+%let all the theta be zero initially
+theta=zeros(size(X,2),1);
+for i=1:numiter
+  a=X*theta;
+  theta(1,1)=theta(1,1)-alpha*(2*sum(a'*X(:,j)));
+  for j=2:size(theta,1)
+    theta(j,1)=theta(j,1)-alpha*(2*sum(a'*X(:,j))+lambda*abs(theta(j,1)));
+  end;
+end;
